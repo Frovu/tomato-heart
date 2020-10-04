140772b97defcf0cee1e984492adada8d27101ff
Frovy <sfrovis@gmail.com>
2020-10-04 20:00:12 +0300

    refactor: test: settings

---
+++ heart/__tests__/settings.test.js
@@ -0,0 +1,30 @@
+
+const mock_settings = {
+    settings: {"a": 1},
+    sum: "def"
+};
+
+jest.mock('../settings_default.json', () => mock_settings,
+    { virtual: true });
+jest.mock('../settings.json', () => mock_settings,
+    { virtual: true });
+
+const settings = require('../settings.js');
+
+describe('settings', () => {
+    describe('check hashsum', () => {
+        it('returns true', () => {
+            const resp = settings.check(mock_settings.sum);
+            expect(resp).toEqual(true);
+        })
+        it('returns false', () => {
+            const resp = settings.check("asdsadsa");
+            expect(resp).toEqual(false);
+        })
+        it('updates and changes sum', () => {
+            const resp = settings.update({b: 2});
+            const res = settings.get();
+            expect(res.settings.b).toEqual(2);
+        })
+    });
+});
--- heart/settings.js
+++ heart/settings.js
@@ -2,7 +2,6 @@
 const fs = require('fs');
 const filename = './settings.json';
 const crypto = require('crypto');
-const md5sum = crypto.createHash('md5');
 
 const defaults = require('./settings_default.json');
 const settings = fs.existsSync(filename) ?
@@ -11,12 +10,15 @@ const settings = fs.existsSync(filename) ?
 module.exports.check = (sum) => sum === settings.sum;
 
 module.exports.default = defaults;
+module.exports.get = () => settings;
 
 const save = () =>
     fs.writeFileSync(filename, JSON.stringify(settings, null, 2), 'utf8');
 
-const update = (changes) => {
-    Object.assign(settings, changes);
-    settings.sum = md5sum(JSON.stringify(settings.settings));
+module.exports.update = (changes) => {
+    Object.assign(settings.settings, changes);
+    settings.sum = crypto.createHash('md5')
+        .update(JSON.stringify(settings.settings))
+        .digest('hex');
     save();
 }
