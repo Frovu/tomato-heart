e933a82002d199d520ad4dfcf774508b6d0cc516
Frovy <sfrovis@gmail.com>
2020-11-01 13:15:24 +0300

    add: tests for data insertion

--- heart/__tests__/heart.test.js
+++ heart/__tests__/heart.test.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // support url-encoded
 app.use('/heart', router);
-
+const db = require('../modules/database.js');
 
 describe('heart', () => {
 	let sum = 'empty';
@@ -41,9 +41,46 @@ describe('heart', () => {
 	describe('invalid data insertion', () => {
 		it('responses with 400', async () => {
 			const res = await request(app)
-				.post('/heart')
-				.query({data: {temperature: 42, ad: 'asd'}});
+				.post('/heart/data')
+				.send({t: 42, ad: 'asd'});
 			expect(res.status).toEqual(400);
+		});
+	});
+
+	describe('valid data insertion', () => {
+		const data = {
+			t: 42.42,
+			p: 45.42,
+			h: 42.42,
+			wt1: 42.42,
+			wt2: 99.99,
+			sm1: 42.42,
+			sm2: 42.42,
+			st1: 42.42,
+			st2: 43.42,
+		};
+		const queryFn = jest.spyOn(db.pool, 'query').mockImplementation((q, values)=>{
+			for(const a in data)
+				if(!values.includes(data[a]))
+					return false;
+			return true;
+		});
+		it('works with application/json', async () => {
+			const res = await request(app)
+				.post('/heart/data')
+				.send(data);
+			expect(res.status).toEqual(200);
+			expect(queryFn).toHaveBeenCalled();
+			expect(queryFn).toHaveLastReturnedWith(true);
+		});
+		it('works with x-www-form-urlencoded', async () => {
+			const req = Object.keys(data).map(k => `${k}=${data[k]}`).join('&');
+			const res = await request(app)
+				.post('/heart/data')
+				.send(req);
+			expect(res.status).toEqual(200);
+			expect(queryFn).toHaveBeenCalled();
+			expect(queryFn).toHaveLastReturnedWith(true);
 		});
 	});
 });
