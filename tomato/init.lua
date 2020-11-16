533629200e7404278177c920705bd323c660016a
Frovy <sfrovis@gmail.com>
2020-11-16 12:49:16 +0300

    add: adc init

--- tomato/init.lua
+++ tomato/init.lua
@@ -3,6 +3,11 @@ dofile("http.lua")
 local LED_PIN = 4
 local HEARTBEAT_RATE = 5000
 
+if adc.force_init_mode(adc.INIT_ADC) then
+	node.restart()
+	return
+end
+
 gpio.mode(LED_PIN, gpio.OUTPUT)
 gpio.write(LED_PIN, gpio.HIGH)
 
