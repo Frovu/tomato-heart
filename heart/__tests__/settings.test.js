0870c6a39a585579f0bcdd85662f1dbc8d6f1ede
Frovy <sfrovis@gmail.com>
2020-11-18 14:58:22 +0300

    add: settings validation tests for day prop

--- heart/__tests__/settings.test.js
+++ heart/__tests__/settings.test.js
@@ -31,7 +31,10 @@ describe('settings', () => {
 		const news = () => Object.assign({}, os, changes);
 		const newsp = (prop) => {
 			const nws = Object.assign({}, os);
-			nws[prop] = Object.assign({}, os[prop], changes);
+			nws[prop] = Object.assign({}, os[prop]);
+			for(const dt of ['day', 'night'])
+				nws[prop][dt] = Object.assign([], os[prop][dt]);
+			Object.assign(nws[prop], changes);
 			return nws;
 		};
 		it('returns true if all ok', () => {
@@ -39,6 +42,8 @@ describe('settings', () => {
 			expect(settings.validate(news())).toEqual(true);
 			changes = {day: [13, 14]};
 			expect(settings.validate(newsp('1'))).toEqual(true);
+			changes = {'0': {}};
+			expect(settings.validate(news())).toEqual(false);
 		});
 		it('checks that "on" is bool', () => {
 			changes = {on: 1};
@@ -49,6 +54,16 @@ describe('settings', () => {
 			expect(settings.validate(newsp('1'))).toEqual(true);
 		});
 		it('checks that numbers are numbers', () => {
+			changes = {};
+			let ncw = newsp('1');
+			ncw['1'].day = [];
+			expect(settings.validate(ncw)).toEqual(false);
+			ncw['1'].day = [12, 18];
+			expect(settings.validate(ncw)).toEqual(true);
+			ncw['1'].day = [12];
+			expect(settings.validate(ncw)).toEqual(false);
+			changes = {day: [14]};
+			expect(settings.validate(newsp('0'))).toEqual(false);
 			changes = {day: [11, '12']};
 			expect(settings.validate(newsp('1'))).toEqual(false);
 			changes = {day: [null, 14]};
@@ -79,6 +94,32 @@ describe('settings', () => {
 			expect(settings.validate(newsp('0'))).toEqual(false);
 			changes = {wire: 40};
 			expect(settings.validate(newsp('0'))).toEqual(true);
+		});
+		it('checks that "day" propety feels ok', () => {
+			changes = {day: ['4:20', '16:20']};
+			expect(settings.validate(news())).toEqual(true);
+			changes = {day: ['0:00', '23:59']};
+			expect(settings.validate(news())).toEqual(true);
+			changes = {day: ['0:00', 2359]};
+			expect(settings.validate(news())).toEqual(false);
+			changes = {day: 1e4};
+			expect(settings.validate(news())).toEqual(false);
+			changes = {day: [123]};
+			expect(settings.validate(news())).toEqual(false);
+			changes = {day: ['16:20']};
+			expect(settings.validate(news())).toEqual(false);
+			changes = {day: ['0:00', '24:01']};
+			expect(settings.validate(news())).toEqual(false);
+			changes = {day: ['-0:00', '23:59']};
+			expect(settings.validate(news())).toEqual(false);
+			changes = {day: ['4:00', '3:00']};
+			expect(settings.validate(news())).toEqual(false);
+			changes = {day: ['8:00', '8:00']};
+			expect(settings.validate(news())).toEqual(false);
+			changes = {day: ['8:00', 'a']};
+			expect(settings.validate(news())).toEqual(false);
+			changes = {day: ['+8:00', '20:00']};
+			expect(settings.validate(news())).toEqual(false);
 		});
 	});
 });
