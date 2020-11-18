a43f5f9046179bb1ec57e3cfed77b9eb3945c23d
Frovy <sfrovis@gmail.com>
2020-11-18 14:35:39 +0300

    add: settings validation try/catch

--- heart/modules/settings.js
+++ heart/modules/settings.js
@@ -19,32 +19,36 @@ const save = () =>
 const inRange = (val, range) => typeof val === 'number' && val >= range[0] && val <= range[1];
 
 module.exports.validate = obj => {
-	for(const i of ['heartbeat', 'datarate'])
-		if(!inRange(obj[i], ranges[i]))
-			return false;
-	const day = [];
-	for(const i of [0, 1]) {
-		const v = obj.day[i];
-		if(!v.match(/^\d?\d:\d\d$/)) return false;
-		const time = v.split(':').map(a => parseInt(a));
-		if(isNaN(time[0]) || time[0]<0 || time[0]>23
-			|| isNaN(time[1]) || time[1]<0 || time[0]>59)
-			return false;
-		day[i] = time[0]*60+time[1];
+	try {
+		for(const i of ['heartbeat', 'datarate'])
+			if(!inRange(obj[i], ranges[i]))
+				return false;
+		const day = [];
+		for(const i of [0, 1]) {
+			const v = obj.day[i];
+			if(!v.match(/^\d?\d:\d\d$/)) return false;
+			const time = v.split(':').map(a => parseInt(a));
+			if(isNaN(time[0]) || time[0]<0 || time[0]>23
+				|| isNaN(time[1]) || time[1]<0 || time[0]>59)
+				return false;
+			day[i] = time[0]*60+time[1];
+		}
+		if(day[0] >= day[1]) return false;
+		// per section settings
+		for(const i of ['0', '1']) {
+			for(const j of ['day', 'night'])
+				for(const v of obj[i][j]) // low and up
+					if(!inRange(v, ranges.temp))
+						return false;
+			if(!inRange(obj[i].wire, ranges.wireTemp))
+				return false;
+			if(typeof obj[i].on !== 'boolean')
+				return false;
+		}
+		return true;
+	} catch(e) {
+		return false;
 	}
-	if(day[0] >= day[1]) return false;
-	// per section settings
-	for(const i of ['0', '1']) {
-		for(const j of ['day', 'night'])
-			for(const v of obj[i][j]) // low and up
-				if(!inRange(v, ranges.temp))
-					return false;
-		if(!inRange(obj[i].wire, ranges.wireTemp))
-			return false;
-		if(typeof obj[i].on !== 'boolean')
-			return false;
-	}
-	return true;
 };
 
 module.exports.update = newSettings => {
